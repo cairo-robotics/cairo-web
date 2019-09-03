7a9a39617b81a135fc038555898b651c18a40255
Carl Mueller <cmuell89@gmail.com>
2019-09-03 09:35:32 -0600

    Basic trigger publishing

--- cairo_web/staticfiles/js/cairo_web.js
+++ cairo_web/staticfiles/js/cairo_web.js
@@ -2,14 +2,41 @@ function sleep (time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
+var ros = new ROSLIB.Ros({
+  url : 'ws://localhost:9090'
+});
+
+ros.on('connection', function() {
+  console.log('Connected to websocket server.');
+});
+
+ros.on('error', function(error) {
+  console.log('Error connecting to websocket server: ', error);
+});
+
+ros.on('close', function() {
+  console.log('Connection to websocket server closed.');
+});
 
 $(document).ready(function() {
     $('input[id=toggle1]').change(function() {
         if ($('input[id="toggle1"]').prop('checked') == true){
-          alert("Sending Upright Active data!")    
-          }
+            var triggerPub1 = new ROSLIB.Topic({
+              ros : ros,
+              name : '/mock_trigger_1',
+              messageType : 'std_msgs/Bool',
+            });
+            var bool = new ROSLIB.Message({data : true});
+            triggerPub1.publish(bool);
+        }
         if ($('input[id="toggle1"]').prop('checked') == false){
-          alert("No longer sending Upright Active data!")    
+            var triggerPub1 = new ROSLIB.Topic({
+              ros : ros,
+              name : '/mock_trigger_1',
+              messageType : 'std_msgs/Bool',
+            });
+            var bool = new ROSLIB.Message({data : false});
+            triggerPub1.publish(bool); 
           }
     });
     $('input[id=toggle2]').change(function() {
--- cairo_web/staticfiles/js/cairo_web_rosbridge.js
+++ cairo_web/staticfiles/js/cairo_web_rosbridge.js
@@ -1,134 +1,133 @@
 // Connecting to ROS
   // -----------------
 
-var ros = new ROSLIB.Ros({
-  url : 'ws://localhost:8080'
-});
+// var ros = new ROSLIB.Ros({
+//   url : 'ws://localhost:9090'
+// });
 
-ros.on('connection', function() {
-  console.log('Connected to websocket server.');
-});
+// ros.on('connection', function() {
+//   console.log('Connected to websocket server.');
+// });
 
-ros.on('error', function(error) {
-  console.log('Error connecting to websocket server: ', error);
-});
+// ros.on('error', function(error) {
+//   console.log('Error connecting to websocket server: ', error);
+// });
 
-ros.on('close', function() {
-  console.log('Connection to websocket server closed.');
-});
+// ros.on('close', function() {
+//   console.log('Connection to websocket server closed.');
+// });
 
 // Publishing a Topic
 // ------------------
+$(document).ready(function() {
+  var triggerPub1 = new ROSLIB.Topic({
+    ros : ros,
+    name : '/mock_trigger_1',
+    messageType : 'std_msgs/Bool',
+  });
 
-// Trigger 1
-var triggerPub1 = new ROSLIB.Topic({
-  ros : ros,
-  name : '/mock_trigger_1',
-  messageType : 'std_msgs/Bool',
+  var bool = new ROSLIB.Message({data : true});
+  triggerPub1.subscribe(function(){
+    console.log("Published to Trigger 1!")
+  })
+  triggerPub1.publish(bool);
+
+  var triggerPub2 = new ROSLIB.Topic({
+    ros : ros,
+    name : '/mock_trigger_2',
+    messageType : 'std_msgs/Bool'
+  });
+
+  var bool = new ROSLIB.Message({data : true});
+  triggerPub2.subscribe(function(){
+    console.log("Published to Trigger 2!")
+  })
+  triggerPub2.publish(bool);
+
+  var togglePub1 = new ROSLIB.Topic({
+    ros : ros,
+    name : '/mock_toggle_1',
+    messageType : 'std_msgs/Bool'
+  });
+
+  var bool = new ROSLIB.Message({data : true});
+  togglePub1.subscribe(function(){
+    console.log("Published to Toggle 1!")
+  })
+  togglePub1.publish(bool);
+
+  var togglePub2 = new ROSLIB.Topic({
+    ros : ros,
+    name : '/mock_toggle_2',
+    messageType : 'std_msgs/Bool'
+  });
+
+  var bool = new ROSLIB.Message({data : true});
+  togglePub2.subscribe(function(){
+    console.log("Published to Toggle 2!")
+  })
+  togglePub2.publish(bool);
+
+  // Subscribing to a Topic
+  // ----------------------
+
+  var listener1 = new ROSLIB.Topic({
+    ros : ros,
+    name : '/mock_toggled_1',
+    messageType : 'std_msgs/Bool',
+    throttle_rate : 2000
+  });
+
+  listener1.subscribe(function(message) {
+    console.log('Received message on ' + listener1.name + ': ' + message.data);
+  });
+
+  var listener2 = new ROSLIB.Topic({
+    ros : ros,
+    name : '/mock_toggled_2',
+    messageType : 'std_msgs/Bool',
+    throttle_rate : 2000
+  });
+
+  listener2.subscribe(function(message) {
+    console.log('Received message on ' + listener2.name + ': ' + message.data);
+  });
+
+  // Calling a service
+  // -----------------
+
+  // var addTwoIntsClient = new ROSLIB.Service({
+  //   ros : ros,
+  //   name : '/add_two_ints',
+  //   serviceType : 'rospy_tutorials/AddTwoInts'
+  // });
+
+  // var request = new ROSLIB.ServiceRequest({
+  //   a : 1,
+  //   b : 2
+  // });
+
+  // addTwoIntsClient.callService(request, function(result) {
+  //   console.log('Result for service call on '
+  //     + addTwoIntsClient.name
+  //     + ': '
+  //     + result.sum);
+  // });
+
+  // Getting and setting a param value
+  // ---------------------------------
+
+  // ros.getParams(function(params) {
+  //   console.log(params);
+  // });
+
+  // var maxVelX = new ROSLIB.Param({
+  //   ros : ros,
+  //   name : 'max_vel_y'
+  // });
+
+  // maxVelX.set(0.8);
+  // maxVelX.get(function(value) {
+  //   console.log('MAX VAL: ' + value);
+  // });
 });
-
-var bool = new ROSLIB.Message({data : true});
-triggerPub1.subscribe(function(){
-  console.log("Published to Trigger 1!")
-})
-triggerPub1.publish(bool);
-
-// Trigger 1
-var triggerPub2 = new ROSLIB.Topic({
-  ros : ros,
-  name : '/mock_trigger_2',
-  messageType : 'std_msgs/Bool'
-});
-
-var bool = new ROSLIB.Message({data : true});
-triggerPub2.subscribe(function(){
-  console.log("Published to Trigger 2!")
-})
-triggerPub2.publish(bool);
-
-// Toggle 1
-var togglePub1 = new ROSLIB.Topic({
-  ros : ros,
-  name : '/mock_toggle_1',
-  messageType : 'std_msgs/Bool'
-});
-
-var bool = new ROSLIB.Message({data : true});
-togglePub1.subscribe(function(){
-  console.log("Published to Toggle 1!")
-})
-
-// Trigger 2
-var togglePub2 = new ROSLIB.Topic({
-  ros : ros,
-  name : '/mock_toggle_2',
-  messageType : 'std_msgs/Bool'
-});
-
-var bool = new ROSLIB.Message({data : true});
-togglePub2.subscribe(function(){
-  console.log("Published to Toggle 2!")
-})
-
-// Subscribing to a Topic
-// ----------------------
-
-var listener1 = new ROSLIB.Topic({
-  ros : ros,
-  name : '/mock_toggled_1',
-  messageType : 'std_msgs/Bool',
-  throttle_rate : 1000
-});
-
-listener1.subscribe(function(message) {
-  console.log('Received message on ' + listener1.name + ': ' + message.data);
-});
-
-var listener2 = new ROSLIB.Topic({
-  ros : ros,
-  name : '/mock_toggled_2',
-  messageType : 'std_msgs/Bool',
-  throttle_rate : 1000
-});
-
-listener2.subscribe(function(message) {
-  console.log('Received message on ' + listener2.name + ': ' + message.data);
-});
-
-// Calling a service
-// -----------------
-
-// var addTwoIntsClient = new ROSLIB.Service({
-//   ros : ros,
-//   name : '/add_two_ints',
-//   serviceType : 'rospy_tutorials/AddTwoInts'
-// });
-
-// var request = new ROSLIB.ServiceRequest({
-//   a : 1,
-//   b : 2
-// });
-
-// addTwoIntsClient.callService(request, function(result) {
-//   console.log('Result for service call on '
-//     + addTwoIntsClient.name
-//     + ': '
-//     + result.sum);
-// });
-
-// Getting and setting a param value
-// ---------------------------------
-
-// ros.getParams(function(params) {
-//   console.log(params);
-// });
-
-// var maxVelX = new ROSLIB.Param({
-//   ros : ros,
-//   name : 'max_vel_y'
-// });
-
-// maxVelX.set(0.8);
-// maxVelX.get(function(value) {
-//   console.log('MAX VAL: ' + value);
-// });
